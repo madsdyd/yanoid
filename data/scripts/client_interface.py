31022f515d1dceb91a6975166422ef980ddc08b7
Mads Bondo Dydensborg <mads@dydensborg.dk>
2001-11-22 18:51:46 +0000

    Ups, forgot this file, script interface for client related stuff

---
+++ data/scripts/client_interface.py
@@ -0,0 +1,7 @@
+# This script defines a number of interfaces to be used
+# for "client" like interaction.
+
+# Play a sound
+def PlaySound(soundname):
+    "Play a sound"
+    yanoid_client.PlaySound(soundname)
