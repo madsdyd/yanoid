7780b55ba18ec84db733a80e65d3cba8bdfbfaf9
Mads Bondo Dydensborg <mads@dydensborg.dk>
2001-11-30 21:15:21 +0000

    Made map user some other bricks

--- data/maps/map1.py
+++ data/maps/map1.py
@@ -4,26 +4,21 @@ SetMapName("Classic 1 - by MBD")
 
 # Add a number of bricks
 for x in range(6, 750, 79):
-    for y in range(91, 250, 29):
-        if (y != 207):
-            row = y % 3
-            if row == 0:
-                AddBrick("brick", x, y, "basic_brick_hit()",
-                         "graphics/bricks/blue2_75.png")
-            elif row == 1:
-                AddBrick("brick", x, y, "basic_brick_hit()",
-                         "graphics/bricks/green2_75.png")
-            else:
-                AddBrick("brick", x, y, "basic_brick_hit()",
-                         "graphics/bricks/red2_75.png")
+    for y in range(91, 278, 29):
+        row = y % 7
+        print row
+        if (row == 0 or row == 6):
+            AddBrick("brick", x, y, "basic_brick_hit()",
+                     "graphics/bricks/blue2_75.png")
+        elif (row == 1 or row == 5):
+            AddBrick("brick", x, y, "basic_brick_hit()",
+                     "graphics/bricks/green2_75.png")
+        elif (row == 2 or row == 4):
+            AddBrick("brick", x, y, "basic_brick_hit()",
+                     "graphics/bricks/red2_75.png")
         else:
-            if (x % 2 == 1):
-                AddBrick("brick-stay-3", x, y, "basic_brick_hit()",
-                         "graphics/bricks/yellow_stay_75.png")
-            else:
-                AddBrick("brick", x, y,
-                         "basic_brick_hit()",
-                         "graphics/bricks/red2_75.png")
+            AddBrick("brick-stay-3", x, y, "basic_brick_hit()",
+                     "graphics/bricks/yellow_stay_75.png")
                 
             
 # Add the walls
