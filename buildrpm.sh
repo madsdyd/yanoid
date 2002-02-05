dd2d24ac0fa9c4b9303fa4a9299fd7c99e3dcd1f
Jons Christian Drewsen <jdrewsen@gmail.com>
2002-02-05 16:08:34 +0000

    fixes the buildrom to work on mandrake

--- buildrpm.sh
+++ buildrpm.sh
@@ -9,9 +9,7 @@ cat yanoid.spec.in | sed -e"s/@VERSION@/$YANOIDVERSION/" > yanoid.spec
 make distclean
 cd ..
 tar -czf yanoid-$YANOIDVERSION.tar.gz $PACKDIR
-cp yanoid-$YANOIDVERSION.tar.gz /usr/src/redhat/SOURCES/
+rpm -ta yanoid-$YANOIDVERSION.tar.gz
 popd
-rpm -ta /usr/src/redhat/SOURCES/yanoid-$YANOIDVERSION.tar.gz
-
 # OK all is well - make some cleaning up
 rm -rf /tmp/$PACKDIR
