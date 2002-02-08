304874b986b4dd3bd9ebcc10911dd4c9e916a8e7
Mads Bondo Dydensborg <mads@dydensborg.dk>
2002-02-08 21:06:10 +0000

    Fiddled a bit with the RPM thingy, the spec file is autogenerated and distributed. Fixed a python autoconf problems (+S ;-). Simplified the buildrpm.sh script.

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
+# Figuring out what version is in configure.in
 YANOIDVERSION=`cat configure.in | grep "AM_INIT_AUTOMAKE" | sed -e's/.*"\(.*\)".*/\1/'`
-PACKDIR=yanoid-$YANOIDVERSION
-rm -rf /tmp/$PACKDIR
-mkdir /tmp/$PACKDIR
-cp -r * /tmp/$PACKDIR
-pushd /tmp/$PACKDIR
-cat yanoid.spec.in | sed -e"s/@VERSION@/$YANOIDVERSION/" > yanoid.spec
-make distclean
-cd ..
-tar -czf yanoid-$YANOIDVERSION.tar.gz $PACKDIR
+# Make a distribution
+./configure
+make dist
+# Let RPM do its thing (se the yanoid.spec.in file)
 rpm -ta yanoid-$YANOIDVERSION.tar.gz
-popd
-# OK all is well - make some cleaning up
-rm -rf /tmp/$PACKDIR
