02cb8d81eb7a26661134bf4e4d413be818e387dc
Mads Bondo Dydensborg <mads@dydensborg.dk>
2002-02-12 20:18:13 +0000

    Removed some dead files

--- buildrpm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Figuring out what version is in configure.in
-YANOIDVERSION=`cat configure.in | grep "AM_INIT_AUTOMAKE" | sed -e's/.*"\(.*\)".*/\1/'`
-# Make a distribution
-./configure
-make dist
-# Let RPM do its thing (se the yanoid.spec.in file)
-rpm -ta yanoid-$YANOIDVERSION.tar.gz
